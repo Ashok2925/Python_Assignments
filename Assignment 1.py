0616baa79bef0c4bb17d955f603f41499280ceba
Ashok2925 <joshi.ashok12@gmail.com>
2025-05-12 12:58:58 +0530

    Add files via upload
    Added both the task in this file Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,15 @@
+# Task 1
+x=input('Enter the 1st number: ')
+y=input('Enter the 2nd number: ')
+x=int(x)
+y=int(y)
+print('Addition: ',x+y)
+print('Subtraction: ',x-y)
+print('Multiplication: ',x*y)
+print('Division: ',x/y)
+
+# Task 2
+first_name=input('Enter your 1st name: ')
+last_name=input('Enter your last name: ')
+full_name=first_name+" "+last_name
+print(f"Hello,{full_name},! Welcome to the python Program.")
