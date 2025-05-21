70849ad01574c35f2ce84c477422c37e7b3ef8b0
Ashok2925 <joshi.ashok12@gmail.com>
2025-05-21 08:56:38 +0530

    Add files via upload
    for loop to print 1 to 51 and sum of the total

---
+++ Assignment 2.py
@@ -0,0 +1,12 @@
+# Task 1
+a=int(input('Enter a number: '))
+if a/2:
+    print(a,' is an even number.')
+else:
+    print(a,' is an odd number.')
+
+# Task 2
+Total_Sum=0
+for x in range(1,51):
+    Total_Sum +=x
+print('The sum of numbers 1 to 50 is : ', Total_Sum)
